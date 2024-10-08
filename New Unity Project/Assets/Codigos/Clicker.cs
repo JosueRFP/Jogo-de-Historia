967a69c28c14181df6da36607d5e775acb811357
Josue Ramos Figueiredo Petri <josue.petri.senac@gmail.com>
2024-10-08 14:38:44 -0300

    Mundança do projeto

---
+++ New Unity Project/Assets/Codigos/Clicker.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clicker: MonoBehaviour
+{
+    public int[] navegationUpgrades;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
